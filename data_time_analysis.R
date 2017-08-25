31b81b11c86226b8738c1398077b2c64d4fa131b
PhilippMohr <contact@pmohr.com>
2017-08-25 22:58:26 +0200

    calculate timeunit

---
+++ data_time_analysis.R
@@ -0,0 +1,25 @@
+### a closer look at the time
+
+### table of time differences between bids
+table(diff(bids$time,16))
+# note all are multiples of ~52631578 
+
+### guess how to calculate ~52631578
+print(1000000000/19, 16)
+
+### define timeunit - also done in _init
+timeunit <- 1000000000/19
+
+### plot the time of first two million bids
+plot(bids[1:2000000,"time"])
+
+### highlight repeating wave pattern with vertical lines
+abline(v=750000, col="green")
+abline(v=1500000, col="green")
+
+### calculate length of wave in time units
+(bids[1500000,"time"] - bids[750000,"time"])/timeunit
+
+### note it's about the amount of seconds in a day
+60*60*24
+
