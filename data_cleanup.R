62feec31a6ebbcb6b77610688a7c709346c91eaa
PhilippMohr <contact@pmohr.com>
2017-08-25 20:37:26 +0200

    clean bids and train data

---
+++ data_cleanup.R
@@ -0,0 +1,12 @@
+### data cleanup
+
+### bids ###
+
+### rename empty country
+bids$country[bids$country == ""] <- "none"
+
+
+### train ###
+
+### remove rows where bidder_id is not in bids
+train <- train[train$bidder_id %in% bids$bidder_id,]
