21ffc5bf6b92cadd1f0ba175bdb8845569d0d569
PhilippMohr <contact@pmohr.com>
2017-08-28 18:14:59 +0200

    Added human vs robot stats

---
+++ features_basic_analysis.R
@@ -0,0 +1,24 @@
+### a first look at the basic features
+
+### helper variable for current numeric stats in train dataset
+train.colnames.stats.numeric <- c("number_of_bids", "number_of_auctions", "number_of_merchandise", "number_of_devices", "number_of_countries", "number_of_ips", "number_of_urls", 
+  "time_diff_mean", "time_diff_10_quantile", "time_diff_25_quantile", "quota_fast_bids_t2", "quota_fast_bids_t5", "time_diff_bidder_mean", 
+  "time_diff_bidder_05_quantile", "time_diff_bidder_10_quantile", "time_diff_bidder_25_quantile")
+
+### calc stats for all
+train.mean.all <- apply(train[, train.colnames.stats.numeric], 2, function(x) mean(x, na.rm = TRUE))
+train.median.all <- apply(train[, train.colnames.stats.numeric], 2, function(x) quantile(x, 0.5, na.rm = TRUE))
+
+### calc stats for humans
+train.mean.humans <- apply(train[train$outcome == 0, train.colnames.stats.numeric], 2, function(x) mean(x, na.rm = TRUE))
+train.median.humans <- apply(train[train$outcome == 0, train.colnames.stats.numeric], 2, function(x) quantile(x, 0.5, na.rm = TRUE))
+
+### calc stats for robots
+train.mean.robots <- apply(train[train$outcome == 1, train.colnames.stats.numeric], 2, function(x) mean(x, na.rm = TRUE))
+train.median.robots <- apply(train[train$outcome == 1, train.colnames.stats.numeric], 2, function(x) quantile(x, 0.5, na.rm = TRUE))
+
+### create dataframe
+train.stats.numeric <- data.frame(train.mean.all, train.mean.humans, train.mean.robots, train.median.all, train.median.humans, train.median.robots)
+
+### print data frame
+print(round(train.stats.numeric))
