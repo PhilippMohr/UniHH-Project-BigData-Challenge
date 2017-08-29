0eb9efe733e7b0d18ac3a8d56ab8bb67e80e54b4
PhilippMohr <contact@pmohr.com>
2017-08-29 19:06:32 +0200

    Changed comments

--- data_import.R
+++ data_import.R
@@ -1,7 +1,6 @@
-### download csv-files to your working directory
-### https://www.kaggle.com/c/facebook-recruiting-iv-human-or-bot/data
-
 ### load csv-files into dataframes
+# download csv-files to your working directory
+# https://www.kaggle.com/c/facebook-recruiting-iv-human-or-bot/data
 
 bids <- read.csv("bids.csv", header = TRUE, stringsAsFactors = FALSE)
 train <- read.csv("train.csv", header = TRUE, stringsAsFactors = FALSE)
