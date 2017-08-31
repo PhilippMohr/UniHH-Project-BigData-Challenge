f3360ed42b0061c6bc522805ab998ab0bbb2d982
PhilippMohr <contact@pmohr.com>
2017-08-31 02:32:38 +0200

    Added precision recall and f measure

--- evaluate_roc_curves_of_classification_samples.R
+++ evaluate_roc_curves_of_classification_samples.R
@@ -6,4 +6,14 @@ apply(data.frame(prediction.tree.t70.num,
                  prediction.tree.t70.under.num,
                  prediction.tree.t70.both.num,
                  prediction.tree.t70.rose.num), 
-      2, function(x) roc.curve(train.30$outcome, x))
+      2, function(x) roc.curve(train.30$outcome, x)
+      )
+
+### calculate precision, recall and f measure for all
+apply(data.frame(prediction.tree.t70.num,
+                 prediction.tree.t70.over.num,
+                 prediction.tree.t70.under.num,
+                 prediction.tree.t70.both.num,
+                 prediction.tree.t70.rose.num), 
+      2, function(x) accuracy.meas(train.30$outcome, x)
+      )
