a2e595c9eeb30886e377db404ce403fbb1998ee3
PhilippMohr <contact@pmohr.com>
2017-08-31 02:01:24 +0200

    Fixed function args

--- evaluate_create_trees_and_predictions.R
+++ evaluate_create_trees_and_predictions.R
@@ -1,21 +1,21 @@
 ### create decision trees for various train data sets
 
 ### train.70
-tree.t70.num <- rpart(tree.formula.numeric, train, method = "anova")
-prediction.tree.t70.num <- predict(tree.t70.num, train.30)
+tree.t70.num <- rpart(tree.formula.numeric, train.70, method = "anova")
+prediction.tree.t70.num <- predict(tree.t70.num, newdata = train.30)
 
 ### train.70.over
-tree.t70.over.num <- rpart(tree.formula.numeric, train, method = "anova")
-prediction.tree.t70.over.num <- predict(tree.t70.over.num, train.30)
+tree.t70.over.num <- rpart(tree.formula.numeric, train.70.over, method = "anova")
+prediction.tree.t70.over.num <- predict(tree.t70.over.num, newdata = train.30)
 
 ### train.70.under
-tree.t70.under.num <- rpart(tree.formula.numeric, train, method = "anova")
-prediction.tree.t70.under.num <- predict(tree.t70.under.num, train.30)
+tree.t70.under.num <- rpart(tree.formula.numeric, train.70.under, method = "anova")
+prediction.tree.t70.under.num <- predict(tree.t70.under.num, newdata = train.30)
 
 ### train.70.both
-tree.t70.both.num <- rpart(tree.formula.numeric, train, method = "anova")
-prediction.tree.t70.both.num <- predict(tree.t70.both.num, train.30)
+tree.t70.both.num <- rpart(tree.formula.numeric, train.70.both, method = "anova")
+prediction.tree.t70.both.num <- predict(tree.t70.both.num, newdata = train.30)
 
 ### train.70.rose
-tree.t70.rose.num <- rpart(tree.formula.numeric, train, method = "anova")
-prediction.tree.t70.rose.num <- predict(tree.t70.rose.num, train.30)
+tree.t70.rose.num <- rpart(tree.formula.numeric, train.70.rose, method = "anova")
+prediction.tree.t70.rose.num <- predict(tree.t70.rose.num, newdata = train.30)
