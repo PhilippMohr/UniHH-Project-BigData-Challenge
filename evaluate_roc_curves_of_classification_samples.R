c4bfbeafdcfdd53b321abf5e73765c7e56631950
PhilippMohr <contact@pmohr.com>
2017-08-31 23:23:11 +0200

    Added random forest evaluation

--- evaluate_roc_curves_of_classification_samples.R
+++ evaluate_roc_curves_of_classification_samples.R
@@ -1,13 +1,13 @@
 ### evaluate various decision tree models
 
-### calculate area under curve for all
+### calculate area under curve for all trees
 apply(data.frame(prediction.tree.t70.num,
                  prediction.tree.t70.over.num,
                  prediction.tree.t70.under.num,
                  prediction.tree.t70.both.num,
                  prediction.tree.t70.rose.num), 
       2, function(x) roc.curve(train.30$outcome, x)
-      )
+)
 
 ### calculate precision, recall and f measure for all
 apply(data.frame(prediction.tree.t70.num,
@@ -16,4 +16,22 @@ apply(data.frame(prediction.tree.t70.num,
                  prediction.tree.t70.both.num,
                  prediction.tree.t70.rose.num), 
       2, function(x) accuracy.meas(train.30$outcome, x)
-      )
+)
+
+### calculate area under curve for all random forests
+apply(data.frame(prediction.rforest.t70.num,
+                 prediction.rforest.t70.over.num,
+                 prediction.rforest.t70.under.num,
+                 prediction.rforest.t70.both.num,
+                 prediction.rforest.t70.rose.num), 
+      2, function(x) roc.curve(train.30$outcome, x)
+)
+
+### calculate precision, recall and f measure for all random forests
+apply(data.frame(prediction.rforest.t70.num,
+                 prediction.rforest.t70.over.num,
+                 prediction.rforest.t70.under.num,
+                 prediction.rforest.t70.both.num,
+                 prediction.rforest.t70.rose.num), 
+      2, function(x) accuracy.meas(train.30$outcome, x)
+)
