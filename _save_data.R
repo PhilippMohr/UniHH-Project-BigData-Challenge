4dde712dc54f155d3fbf4252b9e746148deb8bb5
PhilippMohr <contact@pmohr.com>
2017-09-23 18:34:45 +0200

    Added train subsets

--- _save_data.R
+++ _save_data.R
@@ -4,3 +4,11 @@ save(bids, file = "bids.Rda")
 save(train, file = "train.Rda")
 save(test, file = "test.Rda")
 save(samplesubmission, file = "samplesubmission.Rda")
+
+
+save(train.30, file = "train30.Rda")
+save(train.70, file = "train70.Rda")
+save(train.70.over, file = "train70over.Rda")
+save(train.70.under, file = "train70under.Rda")
+save(train.70.both, file = "train70both.Rda")
+save(train.70.rose, file = "train70rose.Rda")
